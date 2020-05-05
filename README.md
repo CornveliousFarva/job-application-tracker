dc9dd4c0c6985bf7eb5bf2ad4bd4440ca4ae8621
Greg Schuman <gcschuman@hotmail.com>
2020-05-05 12:02:16 -0700

    Added the form itself and the file that will contain all stored positions

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # job-application-tracker
 App designed to keep track of jobs applied for by date, location, description, location, and application status.
+
+# Inspiration behind this app
+I was one of many who lost their job as a result of COVID-19, which forced me to collect unemployment. While unemployed, every week, I had to verify that I was still looking for work, even with the pandemic, which meant sending them a list of all jobs applied for every week.
+
+# How To Use 
+The user submits the following information:
+1. The date the application and resume was sent.
+2. The name of the company applied to.
+3. The company's address.
+4. The title applied for
+5. The point of contact
+6. Application status
+7. For personal reasons only-level of interest in the position
---
+++ script.js
@@ -0,0 +1,23 @@
+// //Declare the date
+// const date = document.getElementById("date");
+// //Declare the Name of the Company
+// const company = document.getElementById("companyName");
+// //Declare the address of the company
+// const location = document.getElementById("address")
+// //Declare the title applied for
+// const position = document.getElementById("titleAppliedFor")
+// Declare the point of contact
+// const contact = document.getElementById("personContacted")
+// //Declare the application status
+// const status = document.getElementById("appStatus")
+// //Declare the level of interest in the positon
+// const interest = document.getElementById("levelOfInterest")
+// //Save data
+// localStorage.setItem("date", "companyName", "location", "titleAppliedFor", "levelOfInterest", "appStatus")
+// //Print as a PDF
+// function print (){
+//     const filename = 'PDFFileName.pdf';
+//     html2canvas(document.querySelector('#nodeToRenderAsPDF')).then(canvas =>){
+        
+//     }
+// }
